428cd228a1b7f9094ca20b72fe8ea43c95b30ad9
TJ <TJ@7f82bda0-831b-0410-895c-c142f06d1e64>
2007-08-14 14:22:15 +0000

    TJ/JJ: applied license.
    git-svn-id: file:///vol/seecrbackup/svn/backup-20130322-0303/storage/trunk@5597 7f82bda0-831b-0410-895c-c142f06d1e64

--- storage/storage.py
+++ storage/storage.py
@@ -1,3 +1,26 @@
+## begin license ##
+#
+#    Storage stores data in a reliable, extendable filebased storage
+#    with great performance.
+#    Copyright (C) 2006 Seek You Too B.V. (CQ2) http://www.cq2.nl
+#
+#    This file is part of Storage.
+#
+#    Storage is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    Storage is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Storage; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
 
 from os.path import join, isdir, basename, isfile
 from os import makedirs, rename, popen3, remove, listdir
