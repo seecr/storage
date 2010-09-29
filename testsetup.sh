1ed85144f0b1d6b53dbfa1cf64a54791db0fc17e
TJ <TJ@7f82bda0-831b-0410-895c-c142f06d1e64>
2010-09-29 20:30:32 +0000

    TJ: added testsetup.sh
    git-svn-id: file:///vol/seecrbackup/svn/backup-20130322-0303/storage/trunk@14579 7f82bda0-831b-0410-895c-c142f06d1e64

---
+++ testsetup.sh
@@ -0,0 +1,15 @@
+set -e
+
+rm -rf tmp build
+
+python setup.py install --root tmp
+
+export PYTHONPATH=$(find `pwd` -name '*-packages' -print)
+cp -r test tmp/test
+
+(
+cd tmp/test
+./alltests.py
+)
+
+rm -rf tmp build
