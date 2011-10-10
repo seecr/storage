9924285ab33858b3c2536f68b92917bf4b8dbddf
JJ <JJ@7f82bda0-831b-0410-895c-c142f06d1e64>
2011-10-10 13:27:11 +0000

    HM/JJ: applied license, made test run in both python 2.5 and 2.6
    git-svn-id: file:///vol/seecrbackup/svn/backup-20130322-0303/storage/workingsets/5.1.8-Seecr/version_0-python2.6@17770 7f82bda0-831b-0410-895c-c142f06d1e64

---
+++ test/_alltests.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+## begin license ##
+# 
+# "Storage" stores data in a reliable, extendable filebased storage
+# with great performance. 
+# 
+# Copyright (C) 2006-2008 Seek You Too B.V. (CQ2) http://www.cq2.nl
+# Copyright (C) 2011 Seecr (Seek You Too B.V.) http://seecr.nl
+# 
+# This file is part of "Storage"
+# 
+# "Storage" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# "Storage" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with "Storage"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+## end license ##
+
+import os, sys
+os.system('find .. -name "*.pyc" | xargs rm -f')
+
+from glob import glob
+for path in glob('../deps.d/*'):
+    sys.path.insert(0, path)
+
+sys.path.insert(0, "..")
+
+from unittest import main
+
+from storagetest import StorageTest
+from hierarchicalstoragetest import HierarchicalStorageTest
+
+if __name__ == '__main__':
+    main()
---
+++ test/alltests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+## begin license ##
+# 
+# "Storage" stores data in a reliable, extendable filebased storage
+# with great performance. 
+# 
+# Copyright (C) 2011 Seecr (Seek You Too B.V.) http://seecr.nl
+# 
+# This file is part of "Storage"
+# 
+# "Storage" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# "Storage" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with "Storage"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+## end license ##
+
+export LANG=en_US.UTF-8
+export PYTHONPATH=.:"$PYTHONPATH"
+option=$1
+pyversions="$(pyversions --installed)"
+if [ "${option:0:10}" == "--python2." ]; then
+    shift
+    pyversions="${option:2}"
+fi
+for pycmd in $pyversions; do
+    echo "================ $pycmd _alltests.py $@ ================"
+    $pycmd _alltests.py "$@"
+done
+
+
