4942bde0c631b482fd3ec1b75fd8bd3d091b283c
HM <HM@7f82bda0-831b-0410-895c-c142f06d1e64>
2012-02-27 09:24:31 +0000

    HM/TS: applied license.
    git-svn-id: file:///vol/seecrbackup/svn/backup-20130322-0303/storage/workingsets/5.1.11-Edurep/version_0-purge@19049 7f82bda0-831b-0410-895c-c142f06d1e64

--- test/hierarchicalstoragetest.py
+++ test/hierarchicalstoragetest.py
@@ -4,7 +4,8 @@
 # with great performance. 
 # 
 # Copyright (C) 2006-2010 Seek You Too B.V. (CQ2) http://www.cq2.nl
-# Copyright (C) 2011 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2011-2012 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2012 Stichting Kennisnet http://www.kennisnet.nl
 # 
 # This file is part of "Storage"
 # 
