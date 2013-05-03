07f2b6387f7c8251ba948b5c748e39f1852109f5
Seecr Development Team <development@seecr.nl>
2013-05-03 10:09:39 +0200

    TJ: updated test.

--- test/alltests.sh
+++ test/alltests.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 ## begin license ##
-# 
+#
 # "Storage" stores data in a reliable, extendable filebased storage
-# with great performance. 
-# 
-# Copyright (C) 2011-2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+# with great performance.
+#
+# Copyright (C) 2011-2013 Seecr (Seek You Too B.V.) http://seecr.nl
+#
 # This file is part of "Storage"
-# 
+#
 # "Storage" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Storage" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Storage"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 export LANG=en_US.UTF-8
 export PYTHONPATH=.:"$PYTHONPATH"
+pyversions="python2.6"
+if [ -e /usr/bin/python2.7 ]; then
+    pyversions="$pyversions python2.7"
+fi
 option=$1
-python2.6 _alltests.py "$@"
-
+if [ "${option:0:10}" == "--python2." ]; then
+    shift
+    pyversions="${option:2}"
+fi
+echo Found Python versions: $pyversions
+for pycmd in $pyversions; do
+    echo "================ $t with $pycmd _alltests.py $@ ================"
+    $pycmd _alltests.py "$@"
+done
 
--- testsetup.sh
+++ testsetup.sh
@@ -1,46 +1,51 @@
 ## begin license ##
-# 
+#
 # "Storage" stores data in a reliable, extendable filebased storage
-# with great performance. 
-# 
-# Copyright (C) 2011-2012 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+# with great performance.
+#
+# Copyright (C) 2011-2013 Seecr (Seek You Too B.V.) http://seecr.nl
+#
 # This file is part of "Storage"
-# 
+#
 # "Storage" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Storage" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Storage"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
-set -e
-
+set -o errexit
 rm -rf tmp build
+mydir=$(cd $(dirname $0); pwd)
+source /usr/share/seecr-test/functions
 
-python2.6 setup.py install --root tmp
-
-if [ -f /etc/debian_version ]; then
-    SITE_PACKAGE_DIR=`pwd`/tmp/usr/local/lib/python2.6/dist-packages
-else
-    SITE_PACKAGE_DIR=`pwd`/tmp/usr/lib/python2.6/site-packages
+pyversions="2.6"
+if distro_is_debian_wheezy; then
+    pyversions="2.6 2.7"
 fi
 
-export PYTHONPATH=${SITE_PACKAGE_DIR}:${PYTHONPATH}
+VERSION="x.y.z"
+
+for pyversion in $pyversions; do
+    definePythonVars $pyversion
+    echo "###### $pyversion, $PYTHON"
+    ${PYTHON} setup.py install --root tmp
+done
 cp -r test tmp/test
+removeDoNotDistribute tmp
+find tmp -name '*.py' -exec sed -r -e "
+    s/\\\$Version:[^\\\$]*\\\$/\\\$Version: ${VERSION}\\\$/;
+    " -i '{}' \;
 
-(
-cd tmp/test
-./alltests.sh
-)
-
+cp -r test tmp/test
+runtests "$@"
 rm -rf tmp build
