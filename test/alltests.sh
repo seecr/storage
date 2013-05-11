d0cafd69638da3358bdb6db1339c212405a29eff
Thijs Janssen <thijs@seecr.nl>
2013-05-11 18:31:43 +0200

    TJ: removed \$t

--- test/alltests.sh
+++ test/alltests.sh
@@ -37,7 +37,7 @@ if [ "${option:0:10}" == "--python2." ]; then
 fi
 echo Found Python versions: $pyversions
 for pycmd in $pyversions; do
-    echo "================ $t with $pycmd _alltests.py $@ ================"
+    echo "================ $pycmd _alltests.py $@ ================"
     $pycmd _alltests.py "$@"
 done
 
