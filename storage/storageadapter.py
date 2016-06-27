90862d5eabdad9f426c18acba6004858073f3e4b
Hendrik Morée <hendrik@seecr.nl>
2016-06-27 09:22:08 +0200

    JJ/HM: Added storage adapter for use with OAI

--- storage/storageadapter.py
+++ storage/storageadapter.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python
 ## begin license ##
 #
 # "Storage" stores data in a reliable, extendable filebased storage
 # with great performance.
 #
-# Copyright (C) 2006-2008 Seek You Too B.V. (CQ2) http://www.cq2.nl
-# Copyright (C) 2011-2012, 2016 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2016 Seecr (Seek You Too B.V.) http://seecr.nl
 #
 # This file is part of "Storage"
 #
@@ -25,17 +23,9 @@
 #
 ## end license ##
 
-from os import system                            #DO_NOT_DISTRIBUTE
-system('find .. -name "*.pyc" | xargs rm -f')    #DO_NOT_DISTRIBUTE
-from seecrdeps import includeParentAndDeps       #DO_NOT_DISTRIBUTE
-includeParentAndDeps(__file__, scanForDeps=True) #DO_NOT_DISTRIBUTE
+from meresco.core import Observable
 
-from unittest import main
-
-from storagetest import StorageTest
-from hierarchicalstoragetest import HierarchicalStorageTest
-from storagecomponenttest import StorageComponentTest
-from storageadaptertest import StorageAdapterTest
-
-if __name__ == '__main__':
-    main()
+class StorageAdapter(Observable):
+    def retrieveData(self, identifier, name):
+        raise StopIteration(self.call.getStream(identifier=identifier, partname=name).read())
+        yield
--- test/storageadaptertest.py
+++ test/storageadaptertest.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python
 ## begin license ##
 #
 # "Storage" stores data in a reliable, extendable filebased storage
 # with great performance.
 #
-# Copyright (C) 2006-2008 Seek You Too B.V. (CQ2) http://www.cq2.nl
-# Copyright (C) 2011-2012, 2016 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2016 Seecr (Seek You Too B.V.) http://seecr.nl
 #
 # This file is part of "Storage"
 #
@@ -25,17 +23,20 @@
 #
 ## end license ##
 
-from os import system                            #DO_NOT_DISTRIBUTE
-system('find .. -name "*.pyc" | xargs rm -f')    #DO_NOT_DISTRIBUTE
-from seecrdeps import includeParentAndDeps       #DO_NOT_DISTRIBUTE
-includeParentAndDeps(__file__, scanForDeps=True) #DO_NOT_DISTRIBUTE
+from seecr.test import SeecrTestCase, CallTrace
+from storage.storageadapter import StorageAdapter
+from weightless.core import retval
+from StringIO import StringIO
 
-from unittest import main
 
-from storagetest import StorageTest
-from hierarchicalstoragetest import HierarchicalStorageTest
-from storagecomponenttest import StorageComponentTest
-from storageadaptertest import StorageAdapterTest
+class StorageAdapterTest(SeecrTestCase):
 
-if __name__ == '__main__':
-    main()
+    def testOne(self):
+        adapter = StorageAdapter()
+        observer = CallTrace(returnValues=dict(getStream=StringIO("DATA")))
+        adapter.addObserver(observer)
+
+        result = retval(adapter.retrieveData(identifier='id:1', name='partname'))
+        self.assertEqual('DATA', result)
+        self.assertEqual(['getStream'], observer.calledMethodNames())
+        self.assertEqual({'partname': 'partname', 'identifier': 'id:1'}, observer.calledMethods[0].kwargs)
